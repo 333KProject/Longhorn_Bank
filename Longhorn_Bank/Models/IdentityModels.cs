ce47f2a34a9fd8b442c54c5f171695398df283da
Farrien Khan <farrienkhan@utexas.edu>
2017-05-02 11:46:17 -0500

    Stock quotes

--- Longhorn_Bank/Models/IdentityModels.cs
+++ Longhorn_Bank/Models/IdentityModels.cs
@@ -82,6 +82,8 @@ namespace Longhorn_Bank.Models
         }
 
         public DbSet<AppRole> AppRoles { get; set; }
+
+        public System.Data.Entity.DbSet<Longhorn_Bank.Models.StockQuote> StockQuotes { get; set; }
     }
 
    
