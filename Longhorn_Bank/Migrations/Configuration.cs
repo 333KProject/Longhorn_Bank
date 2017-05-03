81c9029aacefcf9389c949139f1317904651b23d
Farrien Khan <farrienkhan@utexas.edu>
2017-05-03 16:22:40 -0500

    Migrations

--- Longhorn_Bank/Migrations/Configuration.cs
+++ Longhorn_Bank/Migrations/Configuration.cs
@@ -6,12 +6,12 @@ namespace Longhorn_Bank.Migrations
     using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Longhorn_Bank.Models.AppDbContext>
-    {
+    {C:\Users\CNT-Cedar Park-XPS\Documents\Longhorn_Bank\Longhorn_Bank\Longhorn_Bank\Longhorn_Bank\Migrations\Configuration.cs
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
-
+        //Migrations
         protected override void Seed(Longhorn_Bank.Models.AppDbContext context)
         {
             //  This method will be called after migrating to the latest version.
