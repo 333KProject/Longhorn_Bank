c1dc1a281238381c3a8fc01a295dd68c237ffeb1
Jill <jillpatel@utexas.edu>
2017-05-02 22:49:44 -0500

    dont even know at this point

---
+++ Longhorn_Bank/Models/StockPortfolio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Longhorn_Bank.Models
+{
+    public class StockPortfolio
+    {
+       
+        public Int32 StockPortfolioID { get; set; }
+
+        public Int32 StockAccountNumber { get; set; }
+
+        [Required]
+        public Decimal PortfolioCashBalance { get; set; }
+
+        public String Name { get; set; }
+
+        //navigational properties
+     
+     
+        public virtual List<Transaction> Transactions { get; set; }
+        public virtual AppUser User { get; set; }
+        //public virtual List<Bill> Bills { get; set; }
+    }
+}
