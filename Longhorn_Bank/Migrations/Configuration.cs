5f96522671a63689e3130e2f5ee3e50e78d8b401
Farrien Khan <farrienkhan@utexas.edu>
2017-05-03 16:31:04 -0500

    Migrations

--- Longhorn_Bank/Migrations/Configuration.cs
+++ Longhorn_Bank/Migrations/Configuration.cs
@@ -1,3 +1,5 @@
+
+
 namespace Longhorn_Bank.Migrations
 {
     using System;
@@ -6,7 +8,7 @@ namespace Longhorn_Bank.Migrations
     using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Longhorn_Bank.Models.AppDbContext>
-    {C:\Users\CNT-Cedar Park-XPS\Documents\Longhorn_Bank\Longhorn_Bank\Longhorn_Bank\Longhorn_Bank\Migrations\Configuration.cs
+    {    
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
