30be9ab0223a7ca080e471f1b7e1863e58e04837
Farrien Khan <farrienkhan@utexas.edu>
2017-05-05 06:25:57 -0700

    YAY!

---
+++ Longhorn_Bank/Migrations/201705051309331_lastattempt.cs
@@ -0,0 +1,16 @@
+namespace Longhorn_Bank.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class lastattempt : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
